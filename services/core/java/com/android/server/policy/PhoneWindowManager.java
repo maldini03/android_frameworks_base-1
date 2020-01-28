c675874bb5f42105d323a554332b78bd6e6958ee
Sam Mortimer <sam@mortimer.me.uk>
2020-01-28 00:47:27 -0800

    fw/b torch: Let long press power turn torch off when screen is on.
    * When the torch is on, any subsequent long press power is almost certainly
      intended to turn the torch off (regardless of screen state).  Therefore,
      always allow long press power to toggle torch if the torch is on.
    
    * Carbon edits: Adjust to our flashlight code. Thanks to @ezio84 for suggesting to use Settings for isTorchEnabled
    
    * Tested: long press power toggles torch on/off with screen off.
              long press power toggles torch off with screen on and torch on.
              long press power brings up global actions menu with screen on and torch off.
    
    Change-Id: I932caa9f3be06d14408aea2ecb3a6eca73e052e0

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -644,6 +644,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private int mPowerButtonSuppressionDelayMillis = POWER_BUTTON_SUPPRESSION_DELAY_DEFAULT_MILLIS;
 
     private int mTorchActionMode;
+    private boolean mIsTorchEnabled;
 
     private static final int MSG_DISPATCH_MEDIA_KEY_WITH_WAKE_LOCK = 3;
     private static final int MSG_DISPATCH_MEDIA_KEY_REPEAT_WITH_WAKE_LOCK = 4;
@@ -840,6 +841,9 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     UserHandle.USER_ALL);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.SWIPE_TO_SCREENSHOT), false, this,
+                    UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.FLASHLIGHT_ENABLED), false, this,
                     UserHandle.USER_ALL);
             updateSettings();
         }
@@ -1271,7 +1275,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void powerMultiPressAction(long eventTime, boolean interactive, int behavior) {
         switch (behavior) {
             case MULTI_PRESS_POWER_NOTHING:
-                if ((mTorchActionMode == 1) && (!isScreenOn() || isDozeMode())) {
+                if ((mTorchActionMode == 1) && (!isScreenOn() || isDozeMode()
+                        || mIsTorchEnabled)) {
                     toggleFlashLight();
                 }
                 break;
@@ -1420,7 +1425,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (FactoryTest.isLongPressOnPowerOffEnabled()) {
             return LONG_PRESS_POWER_SHUT_OFF_NO_CONFIRM;
         }
-        if ((mTorchActionMode == 2) && (!isScreenOn() || isDozeMode())) {
+        if ((mTorchActionMode == 2) && (!isScreenOn() || isDozeMode() || mIsTorchEnabled)) {
             return LONG_PRESS_POWER_TORCH;
         }
         return mLongPressOnPowerBehavior;
@@ -2226,10 +2231,11 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             mTorchActionMode = Settings.Secure.getIntForUser(resolver,
                     Settings.Secure.TORCH_POWER_BUTTON_GESTURE, 0,
                     UserHandle.USER_CURRENT);
-
             mVolumeMusicControl = Settings.System.getIntForUser(resolver,
                     Settings.System.VOLUME_BUTTON_MUSIC_CONTROL, 1,
                     UserHandle.USER_CURRENT) != 0;
+            mIsTorchEnabled = Settings.Secure.getInt(resolver,
+            		Settings.Secure.FLASHLIGHT_ENABLED, 0) == 1;
         }
         if (updateRotation) {
             updateRotation(true);
