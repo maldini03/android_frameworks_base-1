db426fa1ae44d282557da6870f9ff16b87d7d591
kwaky <kwaky@google.com>
2020-02-06 13:45:57 -0800

    DO NOT MERGE Adjust NotificationView bottom margin based on whether Keyboard is shown.
    If the Keyboard is shown, then margin bottom is 0.
    Otherwise, the margin is the height of the NavBar.
    
    Bug: 149022190
    Test: Manual. Verify that NotificationView's margin adjusts based on
    whether keyboard or IME is shown.
    
    Change-Id: I96c50a4d773dfac9b57ab1c15cfc59d315da4605

--- packages/CarSystemUI/src/com/android/systemui/statusbar/car/CarStatusBar.java
+++ packages/CarSystemUI/src/com/android/systemui/statusbar/car/CarStatusBar.java
@@ -1176,6 +1176,7 @@ public class CarStatusBar extends StatusBar implements CarBatteryController.Batt
         }
 
         mNavigationBarWindow.setVisibility(isKeyboardVisible ? View.GONE : View.VISIBLE);
+        setNotificationViewBottomMargin(isKeyboardVisible ? 0 : mNavigationBarWindow.getHeight());
         mBottomNavBarVisible = !isKeyboardVisible;
     }
 
@@ -1237,6 +1238,13 @@ public class CarStatusBar extends StatusBar implements CarBatteryController.Batt
         addTemperatureViewToController(mNavigationBarView);
         addTemperatureViewToController(mLeftNavigationBarView);
         addTemperatureViewToController(mRightNavigationBarView);
+    }
+
+    private void setNotificationViewBottomMargin(int bottomMargin) {
+        ViewGroup.MarginLayoutParams params =
+                (ViewGroup.MarginLayoutParams) mNotificationView.getLayoutParams();
+        params.setMargins(params.leftMargin, params.topMargin, params.rightMargin, bottomMargin);
+        mNotificationView.setLayoutParams(params);
     }
 
     @Override
