3135b9e2e551cd70762244c37af156baae7bdb71
Lucas Dupin <dupin@google.com>
2020-03-16 15:26:55 -0700

    Do not allow division by 0
    Test: make
    Fixes: 151128233
    Change-Id: Id1a50ae18a2cd014093a17701f6b62070a62e5ff
    Merged-In: Id1a50ae18a2cd014093a17701f6b62070a62e5ff

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -2125,7 +2125,7 @@ public class NotificationPanelView extends PanelView implements
         } else {
             maxHeight = calculatePanelHeightShade();
         }
-        maxHeight = Math.max(maxHeight, min);
+        maxHeight = Math.max(min, maxHeight);
         return maxHeight;
     }
 
