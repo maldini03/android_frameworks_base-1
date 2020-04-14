5fa01ca3084bdee5642721c1de268b5df8a04587
Anushek Prasal <anushekprasal@gmail.com>
2020-04-14 23:41:01 +0530

    base: Swipe up on keyguard to use Face Unlock [1/2]
    Change-Id: I13669b1e575e15873440423e0148dd5744047061

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9834,6 +9834,12 @@ public final class Settings {
         public static final String LOCKSCREEN_SOLID_UNITS_OPACITY = "lockscreen_solid_units_opacity";
 
         /**
+         * Whether face unlock is allowed only on security view.
+         * @hide
+         */
+        public static final String FACE_UNLOCK_ALWAYS_REQUIRE_SWIPE = "face_unlock_always_require_swipe";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
