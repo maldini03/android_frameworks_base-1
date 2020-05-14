8c253fdb4b43b94dd7547b74a6bb423c01de83e6
Rizky Benggolo <travarilo@gmail.com>
2020-05-14 18:52:26 +0000

    NotificationPanelView: Missing "{" sign
    Change-Id: I1b27b7e7eba26e6ab103e0329e3143e3a0fe2ea0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -604,6 +604,8 @@ public class NotificationPanelView extends PanelView implements
     public void onTuningChanged(String key, String newValue) {
         if (DOUBLE_TAP_SLEEP_GESTURE.equals(key)) {
             mDoubleTapToSleepEnabled = newValue == null || Integer.parseInt(newValue) == 1;
+        }
+
         if (STATUS_BAR_QUICK_QS_PULLDOWN.equals(key)) {
             mOneFingerQuickSettingsIntercept = newValue == null ? 1 : Integer.parseInt(newValue);
         }
