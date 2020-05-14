e1907ed4721c83bf54ce44fec4bbff7827c7dcba
Rizky Benggolo <travarilo@gmail.com>
2020-05-14 16:37:20 +0000

    RebootTile: Fix wrong proto
    Change-Id: I9955fd2362b3d4287d2e2b65d5c2c936a542cc66

--- packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/RebootTile.java
@@ -88,7 +88,7 @@ public class RebootTile extends QSTileImpl<BooleanState> {
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.FUTURE_PREFERENCES;
+        return MetricsEvent.FEATURE_PREFERENCES;
     }
 
     @Override
