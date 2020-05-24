ezio84/abc-n_frameworks_base

Add kill app action to nav swipe actions [1/3]

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -641,6 +641,9 @@ public class EdgeBackGestureHandler implements DisplayListener {
             case 12: // Ringer modes
                 Utils.toggleRingerModes(mContext);
                 break;
+            case 13: // Kill app
+                Utils.killForegroundApp();
+                break;
         }
     }
 

maldini03/android_frameworks_base-1

Change-Id: I03b3cfbbe20133982fc04c32470f2901abba97bf

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -673,6 +673,9 @@ public class EdgeBackGestureHandler implements DisplayListener {
             case 12: // Ringer modes
                 ActionUtils.toggleRingerModes(mContext);
                 break;
+            case 13: // Kill app
+                ActionUtils.killForegroundApp();
+                break;
         }
     }
 