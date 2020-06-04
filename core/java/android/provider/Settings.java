b3c7c8904ecfa8cfb319daf21ef2e32856249fe4
Rizky Benggolo <travarilo@gmail.com>
2020-06-04 13:50:13 +0700

    Revert "base: Forward Port Volume Long Press Skip Tracks (1/2)"
    This reverts commit cc00f7bd913727735e0391717a6762456d7ecd0c.
    
    Change-Id: I06a9a4e77fae4e6db0808864a67882e7b3e6c926

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4683,12 +4683,6 @@ public final class Settings {
         public static final String PULSE_TRIGGER_REASON = "pulse_trigger_reason";
 
         /**
-         * Whether or not volume button music controls should be enabled to seek media tracks
-         * @hide
-         */
-        public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
-
-        /**
          * Which Vibration Pattern to use
          * 0: dzzz-dzzz
          * 1: dzzz-da
