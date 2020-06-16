ab75f001eab75ecf0aeed9caa8a71f62bd1a01bf
Rizky Benggolo <travarilo@gmail.com>
2020-06-16 12:42:36 +0700

    Revert "Introduce Paranoid FaceSense"
    This reverts commit a38918623b360071b415fff246d8034433f690ec.

--- services/core/java/com/android/server/biometrics/BiometricServiceBase.java
+++ services/core/java/com/android/server/biometrics/BiometricServiceBase.java
@@ -696,7 +696,7 @@ public abstract class BiometricServiceBase extends SystemService
      * Callback handlers from the daemon. The caller must put this on a handler.
      */
 
-    public void handleAcquired(long deviceId, int acquiredInfo, int vendorCode) {
+    protected void handleAcquired(long deviceId, int acquiredInfo, int vendorCode) {
         ClientMonitor client = mCurrentClient;
         if (client != null && client.onAcquired(acquiredInfo, vendorCode)) {
             removeClient(client);
@@ -708,7 +708,7 @@ public abstract class BiometricServiceBase extends SystemService
         }
     }
 
-    public void handleAuthenticated(BiometricAuthenticator.Identifier identifier,
+    protected void handleAuthenticated(BiometricAuthenticator.Identifier identifier,
             ArrayList<Byte> token) {
         ClientMonitor client = mCurrentClient;
         final boolean authenticated = identifier.getBiometricId() != 0;
@@ -725,7 +725,7 @@ public abstract class BiometricServiceBase extends SystemService
         }
     }
 
-    public void handleEnrollResult(BiometricAuthenticator.Identifier identifier,
+    protected void handleEnrollResult(BiometricAuthenticator.Identifier identifier,
             int remaining) {
         ClientMonitor client = mCurrentClient;
         if (client != null && client.onEnrollResult(identifier, remaining)) {
@@ -738,7 +738,7 @@ public abstract class BiometricServiceBase extends SystemService
         }
     }
 
-    public void handleError(long deviceId, int error, int vendorCode) {
+    protected void handleError(long deviceId, int error, int vendorCode) {
         final ClientMonitor client = mCurrentClient;
 
         if (DEBUG) Slog.v(getTag(), "handleError(client="
@@ -764,7 +764,7 @@ public abstract class BiometricServiceBase extends SystemService
         }
     }
 
-    public void handleRemoved(BiometricAuthenticator.Identifier identifier,
+    protected void handleRemoved(BiometricAuthenticator.Identifier identifier,
             final int remaining) {
         if (DEBUG) Slog.w(getTag(), "Removed: fid=" + identifier.getBiometricId()
                 + ", dev=" + identifier.getDeviceId()
@@ -790,7 +790,7 @@ public abstract class BiometricServiceBase extends SystemService
         }
     }
 
-    public void handleEnumerate(BiometricAuthenticator.Identifier identifier, int remaining) {
+    protected void handleEnumerate(BiometricAuthenticator.Identifier identifier, int remaining) {
         ClientMonitor client = getCurrentClient();
 
         if(client == null) return;
@@ -1149,7 +1149,7 @@ public abstract class BiometricServiceBase extends SystemService
     /**
      * Populates existing authenticator ids. To be used only during the start of the service.
      */
-    public void loadAuthenticatorIds() {
+    protected void loadAuthenticatorIds() {
         // This operation can be expensive, so keep track of the elapsed time. Might need to move to
         // background if it takes too long.
         long t = System.currentTimeMillis();
@@ -1229,7 +1229,7 @@ public abstract class BiometricServiceBase extends SystemService
      * This method should be called upon connection to the daemon, and when user switches.
      * @param userId
      */
-    public void doTemplateCleanupForUser(int userId) {
+    protected void doTemplateCleanupForUser(int userId) {
         if (mCleanupUnusedFingerprints) {
             enumerateUser(userId);
         }
@@ -1293,7 +1293,7 @@ public abstract class BiometricServiceBase extends SystemService
         doTemplateCleanupForUser(userId);
     }
 
-    public void notifyLockoutResetMonitors() {
+    protected void notifyLockoutResetMonitors() {
         for (int i = 0; i < mLockoutMonitors.size(); i++) {
             mLockoutMonitors.get(i).sendLockoutReset();
         }
