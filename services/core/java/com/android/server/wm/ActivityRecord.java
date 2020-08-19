adinkwok/android_frameworks_base_forked

fwb: Add ability to permanently hide apps from recents [1/3]
This is based off XposedAppSettings, but modified to fit our needs, and
a little bit of: d9973ba
Part 2 is inside the CarbonFibers Bringup

Squashed with fwb: fix HIDE_FROM_RECENTS_LIST provider entry

Change-Id: I859f2c9561fc7fc77c57d8dad5f1ba6a8033e21b

services/core/java/com/android/server/am/ActivityRecord.java
@@ -145,6 +145,8 @@ import android.app.servertransaction.ResumeActivityItem;
 import android.app.servertransaction.WindowVisibilityItem;
 import android.app.servertransaction.ActivityConfigurationChangeItem;
 import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
@@ -167,6 +169,7 @@ import android.os.Trace;
 import android.os.UserHandle;
 import android.os.storage.StorageManager;
 import android.service.voice.IVoiceInteractionSession;
+import android.provider.Settings;
 import android.util.EventLog;
 import android.util.Log;
 import android.util.MergedConfiguration;
@@ -918,6 +921,29 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
             realTheme = aInfo.applicationInfo.targetSdkVersion < HONEYCOMB
                     ? android.R.style.Theme : android.R.style.Theme_Holo;
         }
+
+        String pkgName = aInfo.packageName;
+        String hideFromRecentsString = Settings.System.getStringForUser(service.mContext.getContentResolver(),
+                Settings.System.HIDE_FROM_RECENTS_LIST, UserHandle.USER_CURRENT);
+        ArrayList<String> excludeFromRecentsList = new ArrayList();
+
+        // this converts the String we get from Settings to an actual ArrayList
+        if (hideFromRecentsString!=null && hideFromRecentsString.length()!=0){
+            String[] parts = hideFromRecentsString.split("\\|");
+            for(int i = 0; i < parts.length; i++){
+                excludeFromRecentsList.add(parts[i]);
+            }
+        }
+
+        if (!excludeFromRecentsList.isEmpty()){
+            if (excludeFromRecentsList.contains(pkgName)) {
+              // If our app is inside the ArrayList, hide it from the Recents.
+              // For the case where that flag already was added by some other instance,
+              // it most likely has a good reason to be, so do not force remove it
+              intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+            }
+        }
+
         if ((aInfo.flags & ActivityInfo.FLAG_HARDWARE_ACCELERATED) != 0) {
             windowFlags |= LayoutParams.FLAG_HARDWARE_ACCELERATED;
         }

maldini03/android_frameworks_base-1

services/core/java/com/android/server/wm/ActivityRecord.java
@@ -172,6 +172,8 @@ import android.app.servertransaction.TopResumedActivityChangeItem;
 import android.app.servertransaction.WindowVisibilityItem;
 import android.app.usage.UsageEvents.Event;
 import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
@@ -192,6 +194,7 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.UserHandle;
 import android.os.storage.StorageManager;
+import android.provider.Settings;
 import android.service.voice.IVoiceInteractionSession;
 import android.util.BoostFramework;
 import android.util.EventLog;
@@ -1016,6 +1019,29 @@ public final class ActivityRecord extends ConfigurationContainer {
             realTheme = aInfo.applicationInfo.targetSdkVersion < HONEYCOMB
                     ? android.R.style.Theme : android.R.style.Theme_Holo;
         }
+
+        String pkgName = aInfo.packageName;
+        String hideFromRecentsString = Settings.System.getStringForUser(mAtmService.mContext.getContentResolver(),
+                Settings.System.HIDE_FROM_RECENTS_LIST, UserHandle.USER_CURRENT);
+        ArrayList<String> excludeFromRecentsList = new ArrayList();
+
+        // this converts the String we get from Settings to an actual ArrayList
+        if (hideFromRecentsString!=null && hideFromRecentsString.length()!=0){
+            String[] parts = hideFromRecentsString.split("\\|");
+            for(int i = 0; i < parts.length; i++){
+                excludeFromRecentsList.add(parts[i]);
+            }
+        }
+
+        if (!excludeFromRecentsList.isEmpty()){
+            if (excludeFromRecentsList.contains(pkgName)) {
+              // If our app is inside the ArrayList, hide it from the Recents.
+              // For the case where that flag already was added by some other instance,
+              // it most likely has a good reason to be, so do not force remove it
+              intent.addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+            }
+        }
+
         if ((aInfo.flags & ActivityInfo.FLAG_HARDWARE_ACCELERATED) != 0) {
             windowFlags |= LayoutParams.FLAG_HARDWARE_ACCELERATED;
         }